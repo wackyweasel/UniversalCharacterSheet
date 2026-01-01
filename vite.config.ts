f274b6e30a3baf84bc5c968e09e4721de42602f6
Jean-Luc Deziel <jean-luc.deziel@bentley.com>
2026-01-01 13:51:45 -0500

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/UniversalCharacterSheet/',
+  base: process.env.VITE_BASE_URL || '/UniversalCharacterSheet/',
   plugins: [react()],
   resolve: {
     alias: {
